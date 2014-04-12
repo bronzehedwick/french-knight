03357e429af7d352cf2a0e749063cf64d388b57d
Chris DeLuca <bronzehedwick@gmail.com>
2014-04-12 12:28:01 -0400

    Add gulp build task.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,26 @@
 var gulp = require('gulp');
 
-gulp.task('default', function() {
-  // place code for your default task here
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+
+var paths = {
+  scripts: 'js/src/*js',
+};
+
+gulp.task('scripts', function() {
+  'use strict';
+  // Minify and copy all JavaScript (except vendor scripts)
+  return gulp.src(paths.scripts)
+    .pipe(uglify())
+    .pipe(concat('all.min.js'))
+    .pipe(gulp.dest('js/build'));
 });
+
+// Rerun the task when a file changes
+gulp.task('watch', function() {
+  'use strict';
+  gulp.watch(paths.scripts, ['scripts']);
+});
+
+// The default task (called when you run `gulp` from cli)
+gulp.task('default', ['scripts']);
