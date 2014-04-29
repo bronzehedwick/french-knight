a135fd1b13891580aa1cc3af8a194ed26de0344b
Chris DeLuca <bronzehedwick@gmail.com>
2014-04-28 22:45:46 -0400

    Soft code listening port for heroku.

--- app.js
+++ app.js
@@ -6,5 +6,8 @@ var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
-//have our app listen on port 5000
-app.listen(5000);
+//have our app listen on port 3000
+app.listen(process.env.PORT || 3000, function(){
+  'use strict';
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
