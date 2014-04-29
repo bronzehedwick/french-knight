734ec9d5f6306afe46a3c81cb1e74ab4c3b236c7
Chris DeLuca <bronzehedwick@gmail.com>
2014-04-28 22:33:21 -0400

    Get code ready for heroku.

--- app.js
+++ app.js
@@ -6,6 +6,5 @@ var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
-//have our app listen on port 3000
-app.listen(3000);
-console.log('Your app is now running at: http://127.0.0.1:3000/');
+//have our app listen on port 5000
+app.listen(5000);
